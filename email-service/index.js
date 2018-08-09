6f328f4f8b498a8f19454cc9e11958882c5d8c1d
Derek Rusu <derek.rusu@gmail.com>
2018-08-08 22:37:54 -0500

    Testing out email service, modified the request structure and the json web token process (slightly). Added new env values, added ignore for .env (not getting my keys :p)

--- email-service/index.js
+++ email-service/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv').config();
 var bodyParser = require('body-parser');
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.port || 8080;
 
 
 
@@ -21,14 +22,14 @@ class EmailServiceClient {
           // parse application/json
           app.use(bodyParser.json())
 
-          //verify tokens
+          // verify tokens
           app.use((req, res, next)=>{
             if(req.path.includes('token')){
               next();
               return;
             }
-            if(!req.query.token){res.status(404).send('This page does not exist. Down for maintenance'); return;}
-              jwt.verify(req.query.token, process.env.key, function(err, decoded) {
+            if(!req.body.token){res.status(404).send('This page does not exist. Down for maintenance'); return;}
+            jwt.verify(req.body.token, process.env.secret, function(err, decoded) {
               if(err) {
                 return res.status(404).send('This page does not exist. Down for maintenance');
               }
@@ -38,32 +39,29 @@ class EmailServiceClient {
 
           app.get('/token', (req, res, next)=>{
             res.json({
-              "token": jwt.sign(process.env.payload, process.env.key)
+              'token': jwt.sign(req.query.token, process.env.secret)
             });
           })
 
           app.post('/sendemail', (req, res, next)=>{
 
             var formData = {
-              // Pass a simple key-value pair
               from: req.body.from,
-              // Pass data via Buffers
               to: req.body.to,
               subject: req.body.subject,
               text: req.body.text
             };
 
-            var request = require('request')
-            const username = process.env.username;
-            const password = process.env.password;
-            const url = "https://api.mailgun.net/v3/sandbox933c2e80fc374d9998083f771a420c84.mailgun.org/messages";
-            const auth = "Basic " + new Buffer(username + ":" + password).toString("base64");
+            var request = require('request');
+            const key = process.env.key;
+            const url = process.env.url + process.env.domain + '/messages'
+            const auth = 'Basic ' + new Buffer( 'api:' + key).toString('base64');
 
             request({
               url : url,
               method: 'POST',
               headers : {
-                  "Authorization" : auth
+                  'Authorization' : auth
               },
               formData: formData
             },
@@ -75,7 +73,8 @@ class EmailServiceClient {
                 });
               }
               res.json({
-                message: "Email successfully sent!"
+                status: 'Success',
+                message: body
               })
             });
           });
