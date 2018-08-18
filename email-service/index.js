eca381cf1fe37513ea42a8fcfa76709287daeba4
Derek Rusu <derek.rusu@gmail.com>
2018-08-18 16:59:12 -0500

    [BUGFIX] Guess i do need CORS for heroku, lol, learning through trial an error

--- email-service/index.js
+++ email-service/index.js
@@ -17,7 +17,7 @@ class EmailServiceClient {
         try {
 
           // This is for testing server locally.
-          //app.use(cors());
+          app.use(cors());
 
           // parse application/x-www-form-urlencoded
           app.use(bodyParser.urlencoded({ extended: false }));
